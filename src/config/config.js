e06edacc5aacd015495de7e8cdce89eebfa8884a
AADARSH CHAUDHARY <aadarshchaudhary@AADARSHs-MacBook-Air.local>
2025-11-20 18:42:51 +0545

    chnage or in config js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const mongodb_config = {
-  url: process.env.DB_URI,
-  db_name: process.env.DB_NAME,
+  url: process.env.DB_URI || "",
+  db_name: process.env.DB_NAME || "",
 };
 export const cloudinary_config = {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
